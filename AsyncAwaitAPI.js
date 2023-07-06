8bb88c74379bd91ab12cf9db5c08409cb4ada425
Josh Mwakio <joshua.mwakio@gmail.com>
2023-07-06 22:31:44 +0300

    Changed code with gpt recommendations

--- AsyncAwaitAPI.js
+++ AsyncAwaitAPI.js
@@ -1,27 +1,29 @@
-const getGithubUserProfile=async(username)=>{
-     let response=await fetch(`https://api.github.com/users/${username}`);
-     let data;
-     try {
-        data=await response.json();
-     } catch (error) {
-        console.log(error)
-     }
-     finally{
-        //check if there are objects in the data
-        if(typeof data === 'undefined'){
-            console.log("No data returned");
-        }
-        else{
-        if(data.message === 'Not Found'){
-          console.log("User not found");
-        }
-        else{
-            console.log(`${data.name}\n${data.bio}`);
-        }
+const getGithubUserProfile = async (username) => {
+    try {
+      const response = await fetch(`https://api.github.com/users/${username}`);
+      const data = await response.json();
+  
+      if (typeof data === 'undefined') {
+        throw new Error('No data returned');
+      } else if (data.message === 'Not Found') {
+        throw new Error('User not found');
+      }
+  
+      return {
+        name: data.name,
+        bio: data.bio
+      };
+    } catch (error) {
+      console.log(error);
     }
-
-     }
-    
-}
-
-getGithubUserProfile("joshmwakio");
+  };
+  
+  // Example usage:
+  getGithubUserProfile("joshmwakio")
+    .then((profile) => {
+      console.log(`${profile.name}\n${profile.bio}`);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  
